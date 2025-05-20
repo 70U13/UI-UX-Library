059e8fdcf39d18fce68a5c76e3cdf7642340f2af
Kaye Louise Manilong <kayelouisemanilong@gmail.com>
2025-05-20 21:37:05 +0800

    Added styles in buttons

--- resources/frontend/component/Buttons.jsx
+++ resources/frontend/component/Buttons.jsx
@@ -10,23 +10,22 @@ const Buttons = () => {
         { className: "border-yellow bg-white text-black/80 text-9 button9", text: "Learn More" },
         { className: "border-none shadow1 bg-blue text-14 button14", text: "Sign In" },
         { className: "border-none bg-orange text-black text-2 button2", text: "Continue", hasArrow: true },
+        { className: "border-purple bg-white text-purple text-8 button8", text: "Say Hello!", hasSpan: true},
 
-        { className: "border-purple bg-white text-purple font-style-1 button8", text: "Apply Now", hasSpan: true},
-        { className: "border-blue bg-white text-blue font-style-1 button4", text: "Sign In" },
-        { className: "border-indigo bg-white text-indigo font-style-1 button5", text: "Join Now" },
+        { className: "border-blue bg-white text-blue text-4 button4", text: "Sign In" },
+        { className: "bg-white text-16 button16", text: "Join Now" },
         { className: "border-none bg-orange text-white font-style-1 button11", text: "Continue" },
+        { className: "border-none bg-green text-white font-style-1 button15 ", text: "Submit" },
 
-        { className: "border-none bg-green text-white font-style-1 ", text: "Submit" },
-        { className: "border-none bg-blue text-white font-style-1 button12", text: "Sign In" },
-        { className: "border-none bg-indigo text-white font-style-1", text: "Join Now" },
+        { className: "bg-blue text-white font-style-1 button12", text: "Sign In" },
         { className: "border-none bg-purple text-white font-style-1", text: "Try Free" },
-
         { className: "border-none bg-black text-white font-style-1", text: "Search" },
+
         { className: "border-none shadow1 bg-red text-white font-style-1", text: "Get Started" },
         { className: "border-none shadow1 bg-orange text-white font-style-1", text: "Continue" },
         { className: "border-none shadow1 bg-yellow text-black font-style-1", text: "Learn More" },
-        
         { className: "border-none shadow1 bg-indigo text-white font-style-1", text: "Join Now" },
+
         { className: "border-none shadow1 bg-purple text-white font-style-1", text: "Try Free" },
         { className: "border-none shadow1 bg-black text-white font-style-1", text: "Search" },
     ];
